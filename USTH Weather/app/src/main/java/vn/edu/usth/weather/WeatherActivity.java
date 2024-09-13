33059b30537e67198b7002ddeebe91f2e589e892
Doan Viet <hellobiviet@gmail.com>
2024-09-13 15:19:08 +0700

    Practical Work 7

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,22 +1,30 @@
 package vn.edu.usth.weather;
+
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentPagerAdapter; // Add this import
 import android.util.Log;
+
 public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_forecast, firstFragment).commit();
         WeatherFragment secondFragment = new WeatherFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.weather_condition, secondFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_weather, secondFragment).commit();
         Log.i("create", "onCreate called");
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        WeatherAndForecastPagerAdapter adapter = new WeatherAndForecastPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
     }
 
     @Override
